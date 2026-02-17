61d3b9541b069b622e18bca682c237497436d277
GorangN <gorangnagpal@gmail.com>
2026-02-17 20:48:33 +0100

    Projektdateien hinzufügen.

---
+++ EasyBluetoothAudio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EasyBluetoothAudio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
